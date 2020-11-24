28083c073b5d53db097f91bf23dd3892daaa80b6
Medhi Assabbane <medhi.assabbane@ensiie.fr>
2020-11-24 14:21:15 +0100

    update script

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# ImageScraper
+# ImageScraper :
 Scraping script using python3 and urllib, it downloads all images in a given website.
+
+# description :
+this script has two classes first one gets a max amount of urls  (the variable max_urls = 50 ) and second class searches each url and downloads every picture in that webpage.
+
+# my motivations :
+improve my python skills and share something that could be usefull for others :) 
+please donate here :  [www.assabbane.com]: http://www.assabbane.com/ 
+
+# how to use the script:
+    1. First you shold verrify that you have all the dipendencies , and by dipendencies i mean python3 and the libraries imported in top of the script "imageScraper.py" 
+    2. how to execute the script:
+    * get into the folder containing the script 
+    ```bash
+    cd ImageScraper
+    ```
+    * then execute the script (inside the script change the target website the variable <'site =  "https://www.example.com"'> by your own target or you can just test with the given website "https://www.florajet.com/", if you want to encrease results change the value of 'max_urls' to a higher number )
+    ```python
+    python3 imageScraper.py
+    ```
+    * you can stop the script using ctrl+c or ctrl+z
+
+
+
+# limites of the script and in case it doesn't work:
+
+some website could detect the scraper i already bypassed that using some proxies and headers but i could not put that in the repo since it's not very legal, 
+so if it doesn't work on your target you should be ashamed of what you are doing!!
+
+
+# if i helped you  you can always by me a coffe on : [www.assabbane.com]: http://www.assabbane.com/ 
---
+++ imageScraper.py
@@ -0,0 +1,85 @@
+import urllib.request
+from urllib.parse import urlparse
+import uuid
+
+import re
+import os
+import requests
+from bs4 import BeautifulSoup
+
+max_urls = 50
+site = "https://www.example.com"
+
+
+class UrlScraper:
+    def __init__(self, site, amountOfLinks=-1):
+        self.site = site
+        self.domain = urlparse(site).netloc
+        self.amountOfLinks = amountOfLinks
+        self.urllist = []
+        self.scrape(self.site)
+
+    def scrape(self, link):
+        try:
+            links = re.findall(
+                r"""<\s*a\s*href=["']([^=]+)["']""", urllib.request.urlopen(link).read().decode("utf-8"))
+            for item in links:
+                print(item)
+                if self.domain not in item:
+                    if(str(item).startswith("/")):
+                        pass
+                    else:
+                        continue
+                if 'http' not in item:
+                    item = '{}{}'.format(self.site, item)
+                self.urllist.append(item)
+        except Exception as ex:
+            print(ex)
+
+
+class ImgScraper:
+    def __init__(self):
+        pass
+
+    def scrape(self, site):
+        response = requests.get(site)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        img_tags = soup.find_all('img')
+        urls = []
+        try:
+            for item in img_tags:
+                urls.append(item.get('src'))
+            #urls = [img['src'] for img in img_tags]
+        except Exception as ex:
+            print(ex)
+
+        print(len(urls))
+
+        domain = urlparse(site).netloc
+        print(domain)
+        if not os.path.exists(domain):
+            os.makedirs(domain)
+        for url in urls:
+            filename = re.search(r'/([\w_-]+[.](jpg|gif|jpeg|png))$', str(url))
+            if not filename:
+                print("Regex didn't match with the url: {}".format(url))
+                continue
+            with open(domain+"/"+str(uuid.uuid4())+filename.group(1), 'wb') as f:
+                print("filename ::  "+domain+"/" +
+                      str(uuid.uuid4())+filename.group(1))
+                print(filename.group(1))
+                if 'http' not in url:
+                    # sometimes an image source can be relative
+                    # if it is provide the base url which also happens
+                    # to be the site variable atm.
+                    url = '{}{}'.format(site, url)
+                response = requests.get(url)
+                f.write(response.content)
+
+
+scraper = UrlScraper(site, max_urls)
+imgScraper = ImgScraper()
+
+for ll in scraper.urllist:
+    imgScraper.scrape(ll)
